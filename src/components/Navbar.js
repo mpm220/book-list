92fef286ef0fad97b3fd7d72ea2990ef3f71a184
mpm220 <michaelum215@gmail.com>
2022-05-10 07:30:42 +0100

    elevated states to App.js passed down as props to components which that use them
    Improved structure by letting App.js manage state for the app, simplifying my components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,13 @@
 import React from "react"
 import pic from "../images/Closed_Book_Icon.png"
+import SearchBar from "./SearchBar"
 
-export default function Navbar(){
+export default function Navbar(props){
     return(
         <nav className='navbar'> 
             <img src={pic} className="nav-logo" alt="book-icon"/>
             <span className="nav-title">Reading list.</span>
+            <span className= "searchbar-span"><SearchBar className="search" searchTerm={props.searchTerm} setSearchTerm={props.setSearchTerm} handleSearch={props.handleSearch}></SearchBar></span>
         </nav>
     )
 }
